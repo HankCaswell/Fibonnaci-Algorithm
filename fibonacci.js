3abd9ddf3d32cb7fc587b0d4ae20e7456839003d
Hank Caswell <david.h.caswell@proton.me>
2024-01-31 14:57:05 -0500

    working solution

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,23 @@
 function fibonacci(num) {
-  return 0;
+  if (num <= 1) {
+    return num
+  }
+    else if(num === 2){
+      return 1
+    }
+
+  else {
+    return fibonacci(num-1) + fibonacci(num-2)
+  }
 }
+
+
+console.log(fibonacci(0))
+console.log(fibonacci(3))
+console.log(fibonacci(7))
+
+
+
+
 
 module.exports = fibonacci;
